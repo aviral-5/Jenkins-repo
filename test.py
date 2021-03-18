d8384b5ded8cb1c3a6308da8f0ecb0a5169f3be5
Aviral Srivastava <63145721+aviral-5@users.noreply.github.com>
2021-03-18 17:42:18 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
 print("********Welcome to Jenkins*********")
 print("------This is my first jenkins job------")
-print("Aviral Srivastava"
+#print("Aviral Srivastava")
+print("Aviral Srivastava github id ->aviral-05")
